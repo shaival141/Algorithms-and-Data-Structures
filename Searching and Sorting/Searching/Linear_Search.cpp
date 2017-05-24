a9a794d7c55e7e812c48f8ec7e8ed4f640afb86f
Shaival Chokshi <shaival.chokshi@paytm.com>
2017-05-25 01:59:55 +0530

    Change

--- Searching and Sorting/Searching/Linear_Search.cpp
+++ Searching and Sorting/Searching/Linear_Search.cpp
@@ -11,7 +11,7 @@ int linear_search(int arr[],int n,int key)
 }
 int main()
 {
-	int arr[]={1,2,4,5};
+	int arr[]={1,2,4,5,3,4,5,6};
 	int n=sizeof(arr)/sizeof(arr[0]);
 	int key=2;
 	cout<<linear_search(arr,n,key);
